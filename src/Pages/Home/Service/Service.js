7c17315f4763dce9d8db6b8546dc2628cfb552c3
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-12 02:09:21 +0600

    reponsive with vanilla css

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Service.css';
+
+const Service = ({service}) => {
+    const {name, price, description, img} = service;
+    return (
+        <div className='service-container'>
+                <img src={img} alt="" />
+            <div className='service-description'>
+                <h3>{name}</h3>
+                <p>Price : $ {price}</p>
+                <p>{description}</p>
+                <button>Book : {name}</button>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
+
+/* const services = [
+    {id : 1 , name : 'oil change', price : 100, description : '', img : 'https://i.ibb.co/XzcZDjH/close-up-hands-unrecognizable-mechanic-doing-car-service-maintenance.jpg'}
+] */
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(()=> {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
+
+    return (
+        <>
+        <h2>Services Available : {services.length}</h2>
+        <div className='services-container'>
+            {
+                services.map(service => <Service service={service} key ={service.id}></Service>)
+            }
+        </div>
+        </>
+    );
+};
+
+export default Services;
