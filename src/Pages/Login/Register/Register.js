486cb22f1700ea20866f6cc37a83ad8339d1d880
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-14 14:01:45 +0600

    set environment variable for firebase config

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 import './Register.css';
 
 const Register = () => {
+
+    const handleRegister = event =>{
+        event.preventDefault();
+    }
     return (
         <div className='register-form'>
             <h2 className='text-info text-center my-3'>Please Register!!!</h2>
-            <form>
+            <form onSubmit={handleRegister}>
                 <input type="text" name ='name' placeholder='Your Name' />
                 <input type="email" name="email" id="" placeholder='Email Address' required/>
                 <input type="password" name="password" id="" placeholder='Password' required />
