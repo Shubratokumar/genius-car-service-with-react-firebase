4e33f0e55ba78c6c380d21e1b84042e3a8655607
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-25 12:53:51 +0600

    Order place data send to the db and saved

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -3,12 +3,14 @@ import React from 'react';
 import useServiceDetail from '../../hooks/useServiceDetail';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
-const Checkout = () => {
+function Checkout() {
     // const [serviceId] = useParams();
     const [service] = useServiceDetail();
     const [user] = useAuthState(auth);
-    if(user){
+    if (user) {
         // console.log(user);
     }
     /* const [user, setUser] = useState({
@@ -17,7 +19,6 @@ const Checkout = () => {
         address: "Dhaka Cantonment, Dhaka",
         phone: "01762813923"
     }) */
-
     /* const handleAddressChange = event =>{
         // console.log(event.target.value);
         const {address, ...rest} = user;
@@ -26,37 +27,46 @@ const Checkout = () => {
         console.log(newUser);
         setUser(newUser);
     } */
-
-    const handlePlaceOrder = event =>{
+    const handlePlaceOrder = event => {
         event.preventDefault();
         const order = {
+            name: user.displayName,
             email: user.email,
-            service : service.name,
-            serviceId : service._id,
-            address : event.target.address.value,
-            phone : event.target.phone.value
+            service: service.name,
+            serviceId: service._id,
+            address: event.target.address.value,
+            phone: event.target.phone.value
         }
-    }
+        // use axios for hit the API 
+        axios.post('http://localhost:5000/order', order)
+        .then(response =>{
+            const {data} = response;
+            if(data.insertedId){
+                toast.success("Your order is booked. Our representative will contact with you soon.");
+                event.target.reset();
+            }
+            console.log(response);
+        })
+    };
 
     return (
         <div className='w-50 mx-auto'>
-            <h3>Please Order : {service.name}</h3>
+            <h3>Please Order: {service.name}</h3>
             <form onSubmit={handlePlaceOrder}>
                 <input className='w-100 mb-2' value={user?.displayName} type="text" name='name' placeholder='Your Name' required readOnly disabled />
                 <br />
                 <input className='w-100 mb-2' value={user.email} type="email" name='email' placeholder='Your Email' required readOnly disabled />
                 <br />
-                <input className='w-100 mb-2' value={service.name} type="text" name='service' placeholder='Service Title' required />
+                <input className='w-100 mb-2' value={service.name} type="text" name='service' placeholder='Service Title' required readOnly />
                 <br />
-                <input className='w-100 mb-2'  value={user.address} type="text" name='address'  autoComplete='off' placeholder='Your Address' required />
+                <input className='w-100 mb-2' value={user.address} type="text" name='address' autoComplete='off' placeholder='Your Address' required />
                 <br />
                 <input className='w-100 mb-2' value={user.phone} type="number" name='phone' placeholder='Your Phone Number' required />
                 <br />
                 <input className='btn btn-primary' type="submit" value="Place Order" />
             </form>
-            
         </div>
     );
-};
+}
 
 export default Checkout;
